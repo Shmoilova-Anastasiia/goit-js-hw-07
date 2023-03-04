a5392b439b24af419d042d6b9daf93db797b5f72
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-03-04 18:30:02 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,30 +6,23 @@ const galleryEl = document.querySelector('.gallery');
 
 const makeGallery = galleryItems.map((element) =>{
 
-    const itemGalleryEl = `<a class="gallery__item" href="${element.original}">
-        <img
-          class="gallery__image"
-          src="${element.preview}"
-          alt="${element.description}"
-        />
-      </a>`
+const itemGalleryEl = `<a class="gallery__item" href="${element.original}">
+    <img
+        class="gallery__image"
+        src="${element.preview}"
+        alt="${element.description}"
+    />
+    </a>`
     return itemGalleryEl
-    }).join('')
-    
-        galleryEl.innerHTML = makeGallery;
+}).join('')
+galleryEl.innerHTML = makeGallery;
 
-        galleryEl.addEventListener('click', event => {
-            event.preventDefault();
-            if (event.target.nodeName !== 'IMG'){
-              return
-            }
-            
-            let modal = new SimpleLightbox('.gallery__item', {
-                captionsData:'alt',
-                captionPosition: 'bottom',
-                captionDelay:250,
-            });
+let modal = new SimpleLightbox('.gallery__item', {
+    captionsData:'alt',
+    captionPosition: 'bottom',
+    captionDelay:250,
+});
           
             
-          })
+   
           
