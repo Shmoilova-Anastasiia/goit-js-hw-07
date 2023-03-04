7658d7dd84f67c78eebe379bb229258786845e0c
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-03-04 18:24:48 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const makeGallery = galleryItems.map((element) =>{
+
+    const itemGalleryEl = `<a class="gallery__item" href="${element.original}">
+        <img
+          class="gallery__image"
+          src="${element.preview}"
+          alt="${element.description}"
+        />
+      </a>`
+    return itemGalleryEl
+    }).join('')
+    
+        galleryEl.innerHTML = makeGallery;
+
+        galleryEl.addEventListener('click', event => {
+            event.preventDefault();
+            if (event.target.nodeName !== 'IMG'){
+              return
+            }
+            
+            let modal = new SimpleLightbox('.gallery__item', {
+                captionsData:'alt',
+                captionPosition: 'bottom',
+                captionDelay:250,
+            });
+          
+            
+          })
+          
